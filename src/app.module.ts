a6a4d529626a8c6dc8a2841e089680145dd5623e
Honey Flores <honeyfn0504@gmail.com>
2025-07-15 17:07:25 -0600

    3er Avance del Proyecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,15 @@ import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { NotesModule } from './notes/notes.module';
 import { SummarizerModule } from './summarizer/summarizer.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, NotesModule, SummarizerModule],
+  imports: [AuthModule,PrismaModule,NotesModule,SummarizerModule,UsersModule,],
   providers: [
-    { provide: APP_GUARD, useClass: JwtAuthGuard },
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
   ],
 })
 export class AppModule {}
-
---
+++ src/users/users.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UpdateUserDto } from 'src/users/dto/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(Number(id));
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(Number(id), updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(Number(id));
+  }
+}
