1099966b55a45e07c09c42e65aa993fac65ab607
Honey Flores <honeyfn0504@gmail.com>
2025-07-15 18:09:24 -0600

    3er Avance del Proyecto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,13 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class CreateUserDto {
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'El nombre es obligatorio' })
   name: string;
 
-  @IsEmail()
+  @IsEmail({}, { message: 'El correo debe ser válido' })
   email: string;
+
+  @IsNotEmpty({ message: 'La contraseña es obligatoria' })
+  @MinLength(6, { message: 'La contraseña debe tener al menos 6 caracteres' })
+  password: string;
 }
